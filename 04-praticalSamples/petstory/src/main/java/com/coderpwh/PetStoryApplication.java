04965a6b7c06bbc3fb325a132fdc046cf74d50d1
pengwenhao <pengwenhao@aliyun.com>
2025-09-03 13:24:49 +0800

    feat:add PetStoryApplication

---
+++ 04-praticalSamples/petstory/src/main/java/com/coderpwh/PetStoryApplication.java
@@ -0,0 +1,17 @@
+package com.coderpwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author coderpwh
+ */
+@SpringBootApplication
+public class PetStoryApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetStoryApplication.class, args);
+    }
+
+}
