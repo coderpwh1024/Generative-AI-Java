08c00ea5c23ec82caf85b05ef72868da13679c80
pengwenhao <pengwenhao@aliyun.com>
2025-09-08 21:44:20 +0800

    feat:add McpServerApplication

--- 04-praticalSamples/calculator/src/main/java/com/coderpwh/McpServerApplication.java
+++ 04-praticalSamples/calculator/src/main/java/com/coderpwh/McpServerApplication.java
@@ -1,6 +1,10 @@
 package com.coderpwh;
 
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author coderpwh
@@ -8,6 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class McpServerApplication {
+
+    public static void main(String[] args) {
+        // Start Spring Boot server that will expose calculator tools via MCP
+        SpringApplication.run(McpServerApplication.class, args);
+    }
+
+
+
 
 
 }
