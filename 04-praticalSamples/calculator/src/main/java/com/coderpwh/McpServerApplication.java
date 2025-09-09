315b67339fa6be1671dc7ba55d6a48c5de0e6435
pengwenhao <pengwenhao@aliyun.com>
2025-09-09 12:08:52 +0800

    fix:update McpServerApplication

--- 04-praticalSamples/calculator/src/main/java/com/coderpwh/McpServerApplication.java
+++ 04-praticalSamples/calculator/src/main/java/com/coderpwh/McpServerApplication.java
@@ -1,5 +1,6 @@
 package com.coderpwh;
 
+import com.coderpwh.service.CalculatorService;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
@@ -14,12 +15,19 @@ import org.springframework.context.annotation.Bean;
 public class McpServerApplication {
 
     public static void main(String[] args) {
-        // Start Spring Boot server that will expose calculator tools via MCP
         SpringApplication.run(McpServerApplication.class, args);
     }
 
 
-
+    /***
+     * 注册工具
+     * @param calculatorService
+     * @return
+     */
+    @Bean
+    public ToolCallbackProvider calculatorTools(CalculatorService calculatorService) {
+        return MethodToolCallbackProvider.builder().toolObjects(calculatorService).build();
+    }
 
 
 }
